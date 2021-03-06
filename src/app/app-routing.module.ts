301e72e683975d584398c644687a7a621fe9dca6
renandsb123 <renandsb@gmail.com>
2021-03-06 20:01:00 -0300

    aula1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'categories', loadChildren: () => import(`./pages/categories/categories-routing.module`).then(m => m.CategoriesRoutingModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
