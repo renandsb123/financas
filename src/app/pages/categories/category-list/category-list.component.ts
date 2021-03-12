ccbcbb33c41585e8c7494bfab7cdef894e30b83d
renandsb123 <renandsb@gmail.com>
2021-03-12 19:39:49 -0300

    comitando em ambiente dpara o git

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,3 +1,5 @@
+import { Category } from './../shared/category.model';
+import { CategoryService } from './../shared/category.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) { }
 
-  ngOnInit(): void {
+  categories: Category[] = []
+
+  ngOnInit() {
+    console.log('bbbbbbba',this.categories);
+    // this.categoryService.getAll().subscribe(
+    //   categories => this.categories = categories,
+    //   error => alert('Erro ao carregar a lista')
+    // )
   }
 
+  deleteCategory(category:any) {
+    const mustDelete = confirm('Deseja realmente excluir este item?');
+    
+    console.log('aaaaa',category);
+    if (mustDelete){
+      // this.categoryService.delete(parseInt(category.id)).subscribe(
+      //   () => this.categories = this.categories.filter(element => element != category),
+      //   () => alert("Erro ao tentar excluir!")
+      // )
+    }
+  }
 }
